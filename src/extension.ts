783b7e36a09bf67d823f38ad9f4219989401262b
Peter Staev <peter@tangrasoft.com>
2021-04-04 19:07:15 +0300

    add command to check hub storage status

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import * as serialport from "serialport";
 
 import { Rpc } from "./rpc";
+
+let rpc: Rpc;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,21 +14,56 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand("lego-spikeprime-mindstorms-vscode.helloWorld", async () => {
+	let connectToHubCommand = vscode.commands.registerCommand("lego-spikeprime-mindstorms-vscode.connectToHub", async () => {
+		const ports = await serialport.list();
 		let location = context.globalState.get<string>("lastLocation");
-		location = await vscode.window.showInputBox({ prompt: "Enter location (port, etc):", value: location });
+		const quickPickList = ports.map((item) => ({ label: item.path, picked: item.path === location }));
+		const selection = await vscode.window.showQuickPick(quickPickList, { canPickMany: false });
 
-		if (location) {
+		if (selection) {
+			location = selection.label;
 			context.globalState.update("lastLocation", location);
 
-			const rpc = new Rpc(location);
-			rpc.sendMessage("get_storage_status");
+			rpc = new Rpc(location);
+			await vscode.window.withProgress(
+				{
+					location: vscode.ProgressLocation.Notification,
+					title: "Connecting to Hub...",
+				},
+				() => rpc.open(),
+			);
 		}
-
-
 	});
 
-	context.subscriptions.push(disposable);
+	let disconnectFromHubCommand = vscode.commands.registerCommand("lego-spikeprime-mindstorms-vscode.disconnectFromHub", async () => {
+		if (rpc) {
+			await vscode.window.withProgress(
+				{
+					location: vscode.ProgressLocation.Notification,
+					title: "Disconnecting from Hub...",
+				},
+				() => rpc.close(),
+			);
+		}
+	});
+
+	let getStorageStatusCommand = vscode.commands.registerCommand("lego-spikeprime-mindstorms-vscode.getStorageStatus", async () => {
+		if (rpc) {
+			try {
+				const result = await rpc.sendMessage("get_storage_status");
+
+				console.log(result);
+			}
+			catch (e) {
+				vscode.window.showErrorMessage(e);
+			}
+		}
+	});
+
+
+	context.subscriptions.push(connectToHubCommand);
+	context.subscriptions.push(disconnectFromHubCommand);
+	context.subscriptions.push(getStorageStatusCommand);
 }
 
 // this method is called when your extension is deactivated
--- src/rpc.ts
+++ src/rpc.ts
@@ -1,30 +1,84 @@
 import * as vscode from "vscode";
-import { getuid } from "process";
-import * as SerialPort from "serialport";
+import { v4 as uuidv4 } from "uuid";
+import * as serialport from "serialport";
 
 export class Rpc {
-    private _serialPort: SerialPort;
+    private _serialPort: serialport;
+    private _pendingMessagesPromises = new Map<string, [(result: any) => void, (e: string) => void]>();
 
     constructor(location: string) {
-        this._serialPort = new SerialPort(location, {
-            baudRate: 115200
+        this._serialPort = new serialport(location, {
+            baudRate: 115200,
+            autoOpen: false,
         });
         this._serialPort.setDefaultEncoding("utf-8");
-        this._serialPort.addListener("data", console.log);
-        this._serialPort.addListener("end", console.log);
-        this._serialPort.addListener("error", console.log);
-    }
+        this._serialPort.addListener("data", (data) => {
+            try {
+                const json = JSON.parse(data.toString());
+                const id = json["i"];
+                if (id && this._pendingMessagesPromises.has(id)) {
+                    const [resolve, reject] = this._pendingMessagesPromises.get(id) ?? [];
+                    if (json["r"] && resolve) {
+                        resolve(json["r"]);
+                        return;
+                    }
 
-    public sendMessage(action: string, params: any = {}) {
-        const id = getuid();
-        const msg = { "m": action, "p": params, "i": id };
-        const msgString = JSON.stringify(msg) + "\n";
-        this._serialPort.write(msgString, (err) => {
-            if (err) {
-                vscode.window.showErrorMessage(err.message);
-                return;
+                    if (json["e"] && reject) {
+                        reject(json["e"]);
+                        return;
+                    }
+
+                    this._pendingMessagesPromises.delete(id);
+                }
+            }
+            catch (e) {
+                // DO NOTHING
             }
         });
+        // this._serialPort.addListener("end", console.log);
+        // this._serialPort.addListener("error", console.log);
+    }
 
+    public open(): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            this._serialPort.open((err) => {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    public close(): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            this._serialPort.close((err) => {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+
+                this._serialPort.removeAllListeners();
+            });
+        });
+    }
+
+    public sendMessage(action: string, params: any = {}): Promise<any> {
+        return new Promise((resolve, reject) => {
+            const id = uuidv4();
+            const msg = { "m": action, "p": params, "i": id };
+
+            this._pendingMessagesPromises.set(id, [resolve, reject]);
+            this._serialPort.write(Buffer.from(JSON.stringify(msg) + "\r", "utf-8"), (err) => {
+                if (err) {
+                    reject(err.message);
+                    return;
+                }
+            });
+        });
     }
 }
